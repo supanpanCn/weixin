47a872f4b6e80310632eda8967c41aa77ad8e460
supanpanCn <1372978934@qq.com>
2020-01-07 20:22:40 +0800

    基础

---
+++ pages/demo/demo.js
@@ -0,0 +1,16 @@
+// 必须调用Page方法
+Page({
+  // 相当于vue中的data
+  data: {
+    msg:'我是data中的数据',
+    index:0,
+    list:['vue','react','php'],
+    obj:{
+      "zs":18,
+      "ls":24,
+      "ww":22
+    },
+    is_show:false
+  }
+
+})
