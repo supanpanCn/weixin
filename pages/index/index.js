2f712f43c342c16c45e1d716d90d863f31c3716e
supanpanCn <1372978934@qq.com>
2020-01-09 01:31:41 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,62 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import {request} from './../../request/index.js'
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swiper_list: [],
+    nav_list:[],
+    floor_list:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+  get_data(url,ty=1){
+    request({url})
+      .then(res=>{
+        let key = ''
+        switch(ty){
+          case 1:
+            key = 'swiper_list'
+            break
+          case 2:
+            key='nav_list'
+            break
+          case 3:
+            key='floor_list'
+            break
         }
+        this.setData({
+          [key]:res.data.message
+        })
       })
-    }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  //页面开始加载请求接口，渲染轮播图
+  onLoad: function (options) {
+    this.get_data('https://api.zbztb.cn/api/public/v1/home/swiperdata')
+    this.get_data('https://api.zbztb.cn/api/public/v1/home/catitems',2)
+    this.get_data('https://api.zbztb.cn/api/public/v1/home/floordata',3)
+  },
+  onReady: function () {
+
+  },
+  onShow: function () {
+
+  },
+  onHide: function () {
+
+  },
+  onUnload: function () {
+
+  },
+  onPullDownRefresh: function () {
+
+  },
+  onReachBottom: function () {
+
+  },
+  onShareAppMessage: function () {
+
+  },
+  onPageScroll: function () {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap: function (item) {
+
   }
-})
+});
