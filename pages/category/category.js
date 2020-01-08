d0905265c62abeb22cc5cbd73c45d4b5ba96f549
supanpanCn <1372978934@qq.com>
2020-01-09 04:20:38 +0800

    商品分类

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,60 @@
-// pages/category/category.js
+import {request} from './../../request/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    data_list:[],
+    left_menu:[],
+    right_con:[],
+    active_index:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 判断旧数据是否存在且未过期
+    let cates = wx.getStorageSync('cates');
+    if(!cates){
+      this.getData()
+    }else{
+      if(Date.now()-cates.time>10000*3600){
+        this.getData()
+      }else{
+        this.render_list(cates.data)
+      }
+    }
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getData(){
+    request({url:"https://api.zbztb.cn/api/public/v1/categories"})
+      .then(res=>{
+        let data = res.data.message
+        wx.setStorageSync('cates', {
+          time:Date.now(),
+          data
+        });
+        this.render_list(data)
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  render_list(data){
+    let left_menu = data.map(v=>v.cat_name)
+    let right_con = data[0].children
+    this.setData({
+      right_con,
+      left_menu,
+      data_list:data
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  menu_item_change(ev){
+    let active_index = ev.currentTarget.dataset.active
+    let right_con = this.data.data_list[active_index].children
+    this.setData({
+      right_con,
+      active_index
+    })
   }
+
 })
